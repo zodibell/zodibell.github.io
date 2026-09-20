324c2520c232c742891354172cf72e03cb26fd56
Zodi Bell <zodibell7@gmail.com>
2026-09-20 07:20:47 -0500

    Changing line 4 in import-vocabulary.mjs

--- _scripts/import-vocabulary.mjs
+++ _scripts/import-vocabulary.mjs
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import fs from "fs";
 import path from "path";
-import yaml from "js-yaml";
+import * as yaml from "js-yaml";
 
 /*
  * ============================================================
